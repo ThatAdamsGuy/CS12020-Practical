253816cd205cb3968805673793792869ccdeb2b2
ThatAdamsGuy <harry.adams1997@yahoo.co.uk>
2015-11-18 23:56:29 +0000

    Tree complete
    Currently untested but a tree for all characters is now present.

--- Main Assignment/Main_Assignment/Main_Assignment.ino
+++ Main Assignment/Main_Assignment/Main_Assignment.ino
@@ -195,6 +195,7 @@ void inputStringToMace() {
 
 
 void inputStringToAscii() {
+  asciiString = "";
   maceTree();
   Serial.println(asciiString);
   Serial.println();
@@ -207,62 +208,258 @@ void maceTree() {
       pointer++;
       if (inputString.charAt(pointer) == '=') {
         pointer++;
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          asciiString = asciiString + 'Z';
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + 'W';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + 'V';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'G';
+        }
       }
       else if (inputString.charAt(pointer) == '$') {
         pointer++;
+        if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + 'X';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'N';
+        }
       }
       else if (inputString.charAt(pointer) == '^') {
-
+        pointer++;
+        if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + 'Q';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + 'J';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'D';
+        }
       }
       else {
         pointer++;
         asciiString = asciiString + 'A';
       }
     }
+
+    
     else if (inputString.charAt(pointer) == '$') {
       pointer++;
       if (inputString.charAt(pointer) == '=') {
         pointer++;
-
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          if (inputString.charAt(pointer) == '$') {
+            pointer++;
+            asciiString = asciiString + '=';
+          }
+          else {
+            pointer++;
+            asciiString = asciiString + '?';
+          }
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + '!';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          if (inputString.charAt(pointer) == '^') {
+            pointer++;
+            asciiString = asciiString + '9';
+          }
+          else {
+            pointer++;
+            asciiString = asciiString + "'";
+          }
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'O';
+        }
       }
       else if (inputString.charAt(pointer) == '$') {
         pointer++;
-
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          asciiString = asciiString + '-';
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + '*';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + '+';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'U';
+        }
       }
       else if (inputString.charAt(pointer) == '^') {
         pointer++;
-
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          if (inputString.charAt(pointer) == '=') {
+            pointer++;
+            asciiString = asciiString + '4';
+          }
+          else if (inputString.charAt(pointer) == '$') {
+            pointer++;
+            asciiString = asciiString + '5';
+          }
+          else if (inputString.charAt(pointer) == '^') {
+            pointer++;
+            asciiString = asciiString + '3';
+          }
+          else {
+            pointer++;
+            asciiString = asciiString + ',';
+          }
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          if (inputString.charAt(pointer) == '=') {
+            pointer++;
+            asciiString = asciiString + '7';
+          }
+          else if (inputString.charAt(pointer) == '$') {
+            pointer++;
+            asciiString = asciiString + '8';
+          }
+          else if (inputString.charAt(pointer) == '^') {
+            pointer++;
+            asciiString = asciiString + '6';
+          }
+          else {
+            pointer++;
+            asciiString = asciiString + '/';
+          }
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          if (inputString.charAt(pointer) == '=') {
+            pointer++;
+            asciiString = asciiString + '1';
+          }
+          else if (inputString.charAt(pointer) == '$') {
+            pointer++;
+            asciiString = asciiString + '2';
+          }
+          else if (inputString.charAt(pointer) == '^') {
+            pointer++;
+            asciiString = asciiString + '0';
+          }
+          else {
+            pointer++;
+            asciiString = asciiString + '.';
+          }
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'P';
+        }
       }
       else {
         pointer++;
         asciiString = asciiString + 'T';
       }
     }
+
+
+
     else if (inputString.charAt(pointer) == '^') {
       pointer++;
       if (inputString.charAt(pointer) == '=') {
         pointer++;
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          asciiString = asciiString + 'H';
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + 'C';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + 'F';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'M';
+        }
       }
       else if (inputString.charAt(pointer) == '$') {
         pointer++;
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          asciiString = asciiString + 'R';
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + 'Y';
+        }
+        else if (inputString.charAt(pointer) == '^') {
+          pointer++;
+          asciiString = asciiString + 'S';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'K';
+        }
 
       }
       else if (inputString.charAt(pointer) == '^') {
         pointer++;
-
+        if (inputString.charAt(pointer) == '=') {
+          pointer++;
+          asciiString = asciiString + 'B';
+        }
+        else if (inputString.charAt(pointer) == '$') {
+          pointer++;
+          asciiString = asciiString + 'L';
+        }
+        else {
+          pointer++;
+          asciiString = asciiString + 'I';
+        }
       }
       else {
         pointer++;
         asciiString = asciiString + 'E';
       }
     }
+
+
+    
     else if (inputString.charAt(pointer) == '/') {
       pointer++;            //If char is a slash, not going to be start of a char, so +1 and go again.
     }
+    else if (inputString.charAt(pointer) == ' ') {
+      pointer++;            //Simply a space character
+      asciiString = asciiString + ' ';
+    }
     else {
-      pointer++;            //ERROR DETECTION - If not a =$^/, write invalid character.
+      pointer++;            //ERROR DETECTION - If not a =$^/ or space, write invalid character.
       Serial.println("!!INVALID CHARACTER!!");
     }
   }
 }
+
 
